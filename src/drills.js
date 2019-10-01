62fb9a565012159a162ad315623de498cb1dbfa6
Quasar Wei <quasar.wei@outlook.com>
2019-10-01 08:41:18 -0700

    done

---
+++ src/drills.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+const knex = require('knex');
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL
+});
+
+function searchByName(searchTerm) {
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(result => {
+      console.log(result);
+    });
+}
+
+// searchByName('chicken');
+
+function paginateList(page) {
+  const productsPerPage = 6;
+  const offset = productsPerPage * (page - 1);
+  knexInstance
+    .select('id', 'name', 'price', 'checked', 'category')
+    .from('shopping_list')
+    .limit(productsPerPage)
+    .offset(offset)
+    .then(result => {
+      console.log(result);
+    });
+}
+
+// paginateList(3);
+
+function getItemsBeforeDate(daysAgo) {
+  knexInstance
+    .select('id', 'name', 'price', 'date_added', 'checked', 'category')
+    .from('shopping_list')
+    .where(
+      'date_added',
+      '>',
+      knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo)
+    )
+    .then(result => {
+      console.log(result);
+    })
+}
+
+// getItemsBeforeDate(5);
+
+function getTotalCost() {
+  knexInstance
+    .select('category')
+    .sum('price as total')
+    .from('shopping_list')
+    .groupBy('category')
+    .then(result => {
+      console.log(result);
+    });
+}
+
+getTotalCost();
